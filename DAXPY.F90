756446eb9018fa0a8b96b5dee86dab53eed37cc4
Suzanne Piver <spiver@cheyenne3.ib0.cheyenne.ucar.edu>
2020-01-23 12:45:26 -0700

    DAXPY SIMPLE

--- DAXPY.F90
+++ DAXPY.F90
@@ -1,33 +1,36 @@
-!SuzannePiver test
+!Suzanne Piver
 !DAXPY: compute a constant times a vector plus a vector ax+y
 !N: number of elements in input vector
 !DA:double precidion scalar multiplier
-!DX:double precision vector with N elements	
+!DX:double precision vector with N elements
 !DY:double precision vector with N elements
+
 PROGRAM TEST
 IMPLICIT NONE
-INTERFACE 
-        FUNCTION DAXPY(N, DA,DX,DY)
-                REAL, INTENT(IN) :: N, DA, DX, DY
-                !INTEGER, INTENT(OUT) :: X
-        END FUNCTION DAXPY
-END INTERFACE
-INTEGER :: N = 4
+INTERFACE
+        Subroutine DAXPY(N, DA,DX,DY) 
+                INTEGER, INTENT(IN) :: N, DA
+                REAL, intent(inout) ::  DX(N), DY(N)
+        end subroutine DAXPY
+end interface
+INTEGER, parameter :: N = 4
 INTEGER :: DA = 2
-REAL, dimension(4) :: DX = (/ 1, 1, 1, 1 /)
+REAL :: DX(4) = (/ 1, 1, 1, 1 /)
 REAL :: DY(4) = (/ 1, 1, 1, 1 /)
-Call  DAXPY(N,DA,DX,DY)
+Call DAXPY(N,DA,DX,DY)
 
 END PROGRAM TEST
-FUNCTION DAXPY(N,DA,DX,DY) RESULT(X)
+Subroutine DAXPY(N,DA,DX,DY)
         IMPLICIT NONE
-        INTEGER :: N, DA
-        REAL ::  DX(N), DY(N)
-        REAL :: X(N)
+        INTEGER, intent(in) :: N, DA
+        REAL, intent(inout) ::  DX(N), DY(N)
+        !REAL, dimension(4) :: X
         INTEGER :: i
            do i=1, N
                 DX(i) = DA*DX(i)
-                X(i) = DY(i)+DX(i)
-                Print X(i)
+                DY(i) = DY(i)+DX(i)
+                Print *, DY(i)
            end do
-END FUNCTION DAXPY
+END Subroutine DAXPY
+
+
